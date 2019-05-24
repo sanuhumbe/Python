931794c41c9a632730c81120d2eb5c3de783673a
Sanika Humbe <sanikahumbe@gmail.com>
2019-05-24 15:25:31 +0530

    Update reversenum.py

--- reversenum.py
+++ reversenum.py
@@ -1,3 +1,10 @@
+#1) Let input string be 'str[]' and length of string be 'n'
+#2) l = 0, r = n-1
+#3) While l is smaller than r, do following
+#    a) If str[l] is not an alphabetic character, do l++
+#    b) Else If str[r] is not an alphabetic character, do r--
+#    c) Else swap str[l] and str[r]#
+
 def reverse(str1):
     l1=list(str1)
     n=len(str1)
